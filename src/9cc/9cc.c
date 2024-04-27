73ad4e6ff1bcbf3bbeff9454effab39882d73dc5
bull <oyster880@gmail.com>
2024-04-27 01:32:15 +0000

    wip step4

--- src/9cc/9cc.c
+++ src/9cc/9cc.c
@@ -12,6 +12,26 @@ typedef enum
   TK_EOF,      // End-of-file markers
 } TokenKind;
 
+// 抽象構文木のノードの種類
+typedef enum
+{
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node
+{
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみ使う
+};
 // Token type
 typedef struct Token Token;
 struct Token
@@ -84,6 +104,67 @@ bool at_eof()
 {
   return token->kind == TK_EOF;
 }
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+// Node *mul()
+// {
+//   Node *node = primary();
+
+//   for (;;)
+//   {
+//     if (consume('*'))
+//       node = new_node(ND_MUL, node, primary());
+//     else if (consume('/'))
+//       node = new_node(ND_DIV, node, primary());
+//     else
+//       return node;
+//   }
+// }
+
+// Node *expr()
+// {
+//   Node *node = mul();
+
+//   for (;;)
+//   {
+//     if (consume('+'))
+//       node = new_node(ND_ADD, node, mul());
+//     else if (consume('-'))
+//       node = new_node(ND_SUB, node, mul());
+//     else
+//       return node;
+//   }
+// }
+
+// Node *primary()
+// {
+//   // 次のトークンが"("なら、"(" expr ")"のはず
+//   if (consume('('))
+//   {
+//     Node *node = expr();
+//     expect(')');
+//     return node;
+//   }
+
+//   // そうでなければ数値のはず
+//   return new_node_num(expect_number());
+// }
 
 // Create a new token and add it as the next token of `cur`.
 Token *new_token(TokenKind kind, Token *cur, char *str)
