2541fad58f0c30b6503943e312d539c9c5493d0b
susumu tomita <oyster880@gmail.com>
2024-05-03 15:54:15 +0900

    wip debug

--- src/debug/debug.s
+++ src/debug/debug.s
@@ -1,4 +1,13 @@
-.intel_syntax noprefix
 .globl main
 main:
-push rbp
+// 左辺と右辺をプッシュ
+push 1
+push 2
+
+// 左辺と右辺をRAXとRDIにポップして足す
+pop rdi
+pop rax
+add rax, rdi
+
+// 足した結果をスタックにプッシュ
+push rax
---
+++ src/debug/hello.c
@@ -0,0 +1,9 @@
+// print hellow
+
+#include <stdio.h>
+
+int main()
+{
+  printf("hello\n");
+  return 0;
+}
